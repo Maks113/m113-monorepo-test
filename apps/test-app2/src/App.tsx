ac13a0cc25f5615edeecb4c92c0db3633bfe7d30
Maks Nikolaev <maks113nik@gmail.com>
2021-01-20 20:52:47 +0300

    Add new test project

---
+++ apps/test-app2/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from "@m113-mono-test/button";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button name={'Button name'}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ components/button/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log - @m113-mono-test/button
+
+This log was last generated on Wed, 20 Jan 2021 17:26:17 GMT and should not be manually modified.
+
+## 0.2.0
+Wed, 20 Jan 2021 17:26:17 GMT
+
+### Minor changes
+
+- Add date to button name
+
