37c8536f344b86b5f6da09a62db57c2fa542ae3d
Maks Nikolaev <maks113nik@gmail.com>
2021-01-20 20:22:52 +0300

    Add date to button name

--- components/button/src/index.tsx
+++ components/button/src/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
-
+import { format } from 'date-fns';
 interface IButton {
   name: string;
 }
 
 export const Button = ({ name }: IButton) => {
-  return <button className={'button'}>{name}</button>;
+  return <button className={'button'}>
+    {name}
+    {' '}
+    {format(new Date(), 'dd.MM.yyyy hh:mm:ss')}
+  </button>;
 };
